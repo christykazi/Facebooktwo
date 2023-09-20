7808b871c073f9b2e0eef8243131f811bb68e060
christykazi <christyagbai4178@gmail.com>
2023-09-21 00:01:53 +0100

    ui adjust

--- script.js
+++ script.js
@@ -8,13 +8,16 @@ function settingMenuToggle(){
 darkBtn.onclick = function(){
     darkBtn.classList.toggle("dark-btn-on")  
     document.body.classList.toggle("dark-theme")
-}
+
     if(localStorage.getItem("theme") == "light"){
         localStorage.setItem("theme", "dark") 
     }
     else{
         localStorage.setItem("theme", "light")
     }
+    
+}
+    
 
 
 
